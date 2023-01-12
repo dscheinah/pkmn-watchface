793d0d6a1b70a4714fdebd0cee2eaedae83863ef
cage <dscheinah@googlemail.com>
2023-01-12 15:52:37 +0100

    add some manual processing to some sprites to optimize for pebbles palette

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,12 +16,36 @@ rm -rf ./resources/pokemon/main-sprites/crystal ./resources/pokemon/main-sprites
 pokedex="./src/pkjs/pokedex.json"
 echo '[""' > $pokedex
 
+prefix=./resources/pokemon/main-sprites/gold
+
+if [[ $hasConvert ]]; then
+  convert $prefix/back/2.png -level 0%,100%,0.9 $prefix/back/2.png
+  convert $prefix/back/4.png -level 0%,100%,0.5 $prefix/back/4.png
+  convert $prefix/back/5.png -level 0%,100%,0.9 $prefix/back/5.png
+  convert $prefix/back/6.png -level 0%,100%,0.8 $prefix/back/6.png
+  convert $prefix/back/7.png -level 0%,100%,0.7 $prefix/back/7.png
+  convert $prefix/back/8.png -level 0%,100%,0.7 $prefix/back/8.png
+  convert $prefix/back/142.png -level 0%,100%,1.1 $prefix/back/142.png
+  convert $prefix/back/158.png -level 0%,100%,0.8 $prefix/back/158.png
+  convert $prefix/back/159.png -level 0%,100%,0.7 $prefix/back/159.png
+  convert $prefix/back/shiny/1.png -level 0%,100%,2 $prefix/back/shiny/1.png
+  convert $prefix/back/shiny/4.png -level 0%,100%,0.7 $prefix/back/shiny/4.png
+  convert $prefix/back/shiny/5.png -level 0%,100%,1.4 $prefix/back/shiny/5.png
+  convert $prefix/back/shiny/154.png -level 0%,100%,1.1 $prefix/back/shiny/154.png
+  convert $prefix/back/shiny/160.png -level 0%,100%,1.1 $prefix/back/shiny/160.png
+  convert $prefix/back/shiny/222.png -level 0%,100%,0.3 $prefix/back/shiny/222.png
+  convert $prefix/92.png -level 0%,100%,0.7 $prefix/92.png
+  convert $prefix/93.png -level 0%,100%,1.7 $prefix/93.png
+  convert $prefix/94.png -level 0%,100%,1.5 $prefix/94.png
+  convert $prefix/214.png -level 0%,100%,1.2 $prefix/214.png
+fi
+
 grep main-sprites package.json | xargs -l | cut -d" " -f2 | cut -d"," -f1 | while read -r file; do
   file="./resources/$file"
   fileColor="${file%.*}~color.png"
   fileBw="${file%.*}~bw.png"
   if [[ $hasConvert ]]; then
-    convert "$file" -flatten -alpha off -type palette "$fileColor"
+    convert "$file" -flatten -alpha off +dither -remap ./pebble_colors_64.gif -type palette "$fileColor"
     convert "$file" -flatten -alpha off -monochrome -type palette "$fileBw"
   else
     cp "$file" "${file%.*}~color.png"
@@ -34,7 +58,26 @@ grep main-sprites package.json | xargs -l | cut -d" " -f2 | cut -d"," -f1 | whil
   if [[ "$file" != *"back"* ]]; then
     echo ',"'$(base64 -w0 "$fileColor")'"' >> $pokedex
   fi
-  rm "$file"
 done
+
+if [[ $hasConvert ]]; then
+  convert "$prefix/back/1.png" -flatten -alpha off -colorspace gray -threshold 85% -type bilevel "$prefix/back/1.base.png"
+  convert "$prefix/back/1.png" -flatten -channel rgba -transparent black -fill black -opaque white -fill white -opaque none -alpha off -colorspace gray -threshold 85% -type bilevel "$prefix/back/1.mask.png"
+  convert "$prefix/back/1.base.png" "$prefix/back/1.mask.png" -compose add -composite "$prefix/back/1~bw.png"
+  for i in 155 156 157; do
+    convert "$prefix/back/shiny/$i.png" -flatten -alpha off -colorspace gray -threshold 99% -type bilevel "$prefix/back/shiny/$i.base.png"
+    convert "$prefix/back/shiny/$i.png" -flatten -channel rgba -transparent black -fill black -opaque white -fill white -opaque none -alpha off -colorspace gray -threshold 99% -type bilevel "$prefix/back/shiny/$i.mask.png"
+    convert "$prefix/back/shiny/$i.base.png" "$prefix/back/shiny/$i.mask.png" -compose add -composite "$prefix/back/shiny/$i~bw.png"
+  done
+fi
+if [[ $hasOptiPng ]]; then
+  optipng -strip all -o7 "$prefix/back/1~bw.png"
+  optipng -strip all -o7 "$prefix/back/shiny/155~bw.png"
+  optipng -strip all -o7 "$prefix/back/shiny/156~bw.png"
+  optipng -strip all -o7 "$prefix/back/shiny/157~bw.png"
+fi
+
+find $prefix -type f -not -iname "*~*" -delete
+find $prefix -type d -empty -delete
 
 echo ']' >> $pokedex
