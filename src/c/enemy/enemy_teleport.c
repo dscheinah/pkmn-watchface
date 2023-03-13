e7bae2a9afac8bd080d4a7bf2505b60398dfda04
cage <dscheinah@googlemail.com>
2023-03-13 18:52:06 +0100

    increase normal teleport triggers

--- src/c/enemy/enemy_teleport.c
+++ src/c/enemy/enemy_teleport.c
@@ -44,7 +44,9 @@ void enemy_teleport(State* state) {
     return;
   }
   switch (state->enemy->type) {
+    case RESOURCE_ID_86:
     case RESOURCE_ID_133:
+    case RESOURCE_ID_216:
       createTeleport(state, RESOURCE_ID_63, 1);
       break;
     case RESOURCE_ID_25:
