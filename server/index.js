5b2827ee36baf2e5dbc483bf0ea6d5b7877d2331
cage <dscheinah@googlemail.com>
2024-02-08 23:24:11 +0100

    add dummy ws functionality to server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
+var expressWs = require('express-ws');
 
 var app = express();
+expressWs(app);
+
+var games = [];
 
 app.get('/', function (req, res) {
     res.send('');
+});
+app.ws('/multiplayer', function (ws) {
+    ws.on('start', function (json) {
+        var data = JSON.parse(json);
+        if (games.length) {
+            var game = games.pop();
+            ws.send('data', JSON.stringify({}));
+            game.socket.send('data', JSON.stringify({}));
+            return;
+        }
+        data.socket = ws;
+        games.push(data);
+    });
 });
 
 app.listen(80);
