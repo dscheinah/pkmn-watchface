548a1587dbfd0d62440d8beaa192f2d2daa40101
cage <dscheinah@googlemail.com>
2024-02-11 22:37:35 +0100

    add basic monster and resource maps

--- server/mapper/input.js
+++ server/mapper/input.js
@@ -5,11 +5,341 @@ var types = {
 };
 
 var mapper = {
-    1: {
-        id: 1,
+    '0': {
+        id: '001',
         types: [types.PLANT],
         weak: [types.FIRE],
         resist: [types.PLANT, types.WATER]
+    },
+    '3': {
+        id: '002',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '6': {
+        id: '003',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '1': {
+        id: '004',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '4': {
+        id: '005',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '7': {
+        id: '006',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '2': {
+        id: '007',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '5': {
+        id: '008',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '8': {
+        id: '009',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '9': {
+        id: '025',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '10': {
+        id: '026',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '43': {
+        id: '039',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '44': {
+        id: '040',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '11': {
+        id: '063',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '12': {
+        id: '064',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '13': {
+        id: '065',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '14': {
+        id: '086',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '15': {
+        id: '087',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '16': {
+        id: '092',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '17': {
+        id: '093',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '18': {
+        id: '094',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '53': {
+        id: '125',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '19': {
+        id: '132',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '20': {
+        id: '133',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '21': {
+        id: '134',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '24': {
+        id: '135',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '22': {
+        id: '136',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '46': {
+        id: '143',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '37': {
+        id: '144',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '38': {
+        id: '145',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '39': {
+        id: '146',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '42': {
+        id: '150',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '25': {
+        id: '151',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '34': {
+        id: '172',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '45': {
+        id: '174',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '35': {
+        id: '175',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '36': {
+        id: '176',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '52': {
+        id: '185',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '23': {
+        id: '196',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '26': {
+        id: '197',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '55': {
+        id: '200',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '27': {
+        id: '203',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '28': {
+        id: '213',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '29': {
+        id: '214',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '47': {
+        id: '216',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '48': {
+        id: '217',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '30': {
+        id: '225',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '31': {
+        id: '235',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '54': {
+        id: '239',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '32': {
+        id: '241',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '49': {
+        id: '243',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '50': {
+        id: '244',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '51': {
+        id: '245',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '40': {
+        id: '249',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '41': {
+        id: '250',
+        types: [],
+        weak: [],
+        resist: []
+    },
+    '33': {
+        id: '251',
+        types: [],
+        weak: [],
+        resist: []
     }
 };
 
@@ -23,7 +353,7 @@ module.exports = function (json) {
 
     var monsters = [];
     for (var i = 0; i < 6; i++) {
-        var monster = mapper[input.monsters[i]];
+        var monster = mapper[(input.monsters[i] || '').toString()];
         if (!monster) {
             break;
         }
--- server/mapper/output.js
+++ server/mapper/output.js
@@ -1,7 +1,227 @@
 var mapper = {
-    1: {
+    '001': {
         ally: 1,
-        enemy: 1
+        enemy: 45
+    },
+    '002': {
+        ally: 4,
+        enemy: 48
+    },
+    '003': {
+        ally: 7,
+        enemy: 51
+    },
+    '004': {
+        ally: 2,
+        enemy: 46
+    },
+    '005': {
+        ally: 5,
+        enemy: 49
+    },
+    '006': {
+        ally: 8,
+        enemy: 52
+    },
+    '007': {
+        ally: 3,
+        enemy: 47
+    },
+    '008': {
+        ally: 6,
+        enemy: 50
+    },
+    '009': {
+        ally: 9,
+        enemy: 53
+    },
+    '025': {
+        ally: 105,
+        enemy: 54
+    },
+    '026': {
+        ally: 106,
+        enemy: 55
+    },
+    '039': {
+        ally: 139,
+        enemy: 88
+    },
+    '040': {
+        ally: 140,
+        enemy: 89
+    },
+    '063': {
+        ally: 107,
+        enemy: 56
+    },
+    '064': {
+        ally: 108,
+        enemy: 57
+    },
+    '065': {
+        ally: 109,
+        enemy: 58
+    },
+    '086': {
+        ally: 110,
+        enemy: 59
+    },
+    '087': {
+        ally: 111,
+        enemy: 60
+    },
+    '092': {
+        ally: 112,
+        enemy: 61
+    },
+    '093': {
+        ally: 113,
+        enemy: 62
+    },
+    '094': {
+        ally: 114,
+        enemy: 63
+    },
+    '125': {
+        ally: 149,
+        enemy: 98
+    },
+    '132': {
+        ally: 115,
+        enemy: 64
+    },
+    '133': {
+        ally: 116,
+        enemy: 65
+    },
+    '134': {
+        ally: 117,
+        enemy: 66
+    },
+    '135': {
+        ally: 120,
+        enemy: 69
+    },
+    '136': {
+        ally: 118,
+        enemy: 67
+    },
+    '143': {
+        ally: 142,
+        enemy: 91
+    },
+    '144': {
+        ally: 133,
+        enemy: 82
+    },
+    '145': {
+        ally: 134,
+        enemy: 83
+    },
+    '146': {
+        ally: 135,
+        enemy: 84
+    },
+    '150': {
+        ally: 138,
+        enemy: 87
+    },
+    '151': {
+        ally: 121,
+        enemy: 70
+    },
+    '172': {
+        ally: 130,
+        enemy: 79
+    },
+    '174': {
+        ally: 141,
+        enemy: 90
+    },
+    '175': {
+        ally: 131,
+        enemy: 80
+    },
+    '176': {
+        ally: 132,
+        enemy: 81
+    },
+    '185': {
+        ally: 148,
+        enemy: 97
+    },
+    '196': {
+        ally: 119,
+        enemy: 68
+    },
+    '197': {
+        ally: 122,
+        enemy: 71
+    },
+    '200': {
+        ally: 151,
+        enemy: 100
+    },
+    '203': {
+        ally: 123,
+        enemy: 72
+    },
+    '213': {
+        ally: 124,
+        enemy: 73
+    },
+    '214': {
+        ally: 125,
+        enemy: 74
+    },
+    '216': {
+        ally: 143,
+        enemy: 92
+    },
+    '217': {
+        ally: 144,
+        enemy: 93
+    },
+    '225': {
+        ally: 126,
+        enemy: 75
+    },
+    '235': {
+        ally: 127,
+        enemy: 76
+    },
+    '239': {
+        ally: 150,
+        enemy: 99
+    },
+    '241': {
+        ally: 128,
+        enemy: 77
+    },
+    '243': {
+        ally: 145,
+        enemy: 94
+    },
+    '244': {
+        ally: 146,
+        enemy: 95
+    },
+    '245': {
+        ally: 147,
+        enemy: 96
+    },
+    '249': {
+        ally: 136,
+        enemy: 85
+    },
+    '250': {
+        ally: 137,
+        enemy: 86
+    },
+    '251': {
+        ally: 129,
+        enemy: 78
     }
 };
 
